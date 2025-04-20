5b2fd71250cebeb8be7c2dfc401dc7a8eba673bd
ducanup01 <ducanup01@gmail.com>
2025-04-21 02:46:39 +0700

    first commit

---
+++ code/page5.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("About me")
+st.write("Ripped off version of btd6race")
+
+st.write(
+    """
+    Update history
+
+    Version 1:
+    - v1.0.0 First launch 6/12/24
+    """
+)
---
+++ main.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import streamlit_analytics2 as streamlit_analytics
+
+with streamlit_analytics.track(unsafe_password="ducanup01"):
+    st.set_page_config()
+    st.logo("image/logo.webp")
+    
+
+    home_page = st.Page(
+        page="code/home.py",
+        title="Home",
+        icon=":material/home:",
+    )
+    page_1 = st.Page(
+        page="code/page1.py",
+        title="Boss Activity Leaderboard",
+        icon=":material/leaderboard:",
+        default=True,
+    )
+    page_5 = st.Page(
+        page="code/page5.py",
+        title="About",
+        icon=":material/info:"
+    )
+    pg = st.navigation(
+        {
+        "Home ": [home_page],
+        "Menu ": [page_1],
+        "Index ": [page_5],
+        }
+    )
+    pg.run()
